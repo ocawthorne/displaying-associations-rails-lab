eecad50c925d2eb785e497d9303b5cc402a41562
ocawthorne <ocawthorne@gmail.com>
2020-12-10 23:24:08 +0000

    displaying-associations-rails-lab-121220

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -33,6 +33,23 @@ RSpec.configure do |config|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
 
+  config.backtrace_exclusion_patterns = [
+    # /\/lib\d*\/ruby\//,
+    # /bin\//,
+    # /gems\//,
+    # /spec\/spec_helper\.rb/,
+    # /lib\/rspec\/(core|expectations|matchers|mocks)/
+
+    /\/lib\d*\/ruby\//,
+    /org\/jruby\//,
+    /bin\//,
+    /gems/,
+    /spec\/spec_helper\.rb/,
+    /lib\/rspec\/(core|expectations|matchers|mocks)/
+  ]
+
+  
+
   # rspec-mocks config goes here. You can use an alternate test double
   # library (such as bogus or mocha) by changing the `mock_with` option here.
   config.mock_with :rspec do |mocks|
